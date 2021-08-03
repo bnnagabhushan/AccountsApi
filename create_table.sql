5b4539e9c336232e3e4428dc1ff11958e11baad2
bnnagabhushan <ursbhushan@gmail.com>
2021-08-03 18:37:52 +0530

    DB script added

---
+++ create_table.sql
@@ -0,0 +1,64 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.3 (Debian 13.3-1.pgdg100+1)
+-- Dumped by pg_dump version 13.2
+
+--
+-- Name: postgres; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE postgres WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE postgres OWNER TO postgres;
+
+\connect postgres
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: DATABASE postgres; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON DATABASE postgres IS 'default administrative connection database';
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: Accounts; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Accounts" (
+    "AccountId" character varying(50) NOT NULL,
+    "AccountHolderName" character varying(200),
+    "AccountType" character varying(100)
+);
+
+
+
+--
+-- Name: Accounts Account_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Accounts"
+    ADD CONSTRAINT "Account_pkey" PRIMARY KEY ("AccountId");
+
+
+--
+-- PostgreSQL database dump complete
+--
+
