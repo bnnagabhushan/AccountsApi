fb4df61a17d76ff5e67da23e18ca03976d346cf0
Nagabhushan <nagabhushan.bangalore.nagabasappa@signify.com>
2021-08-03 17:18:50 +0530

    Initial Commit

---
+++ AccountApi/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AccountApi.Models
+{
+    public class Account
+    {
+        public string AccountId { get; set; }
+        public string AccountHolderName { get; set; }
+        public string AccountType { get; set; }
+        public Account()
+        {
+        }
+    }
+}
---
+++ AccountApi/Models/AccountContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountApi.Models
+{
+    public class AccountContext: DbContext
+    {
+        public AccountContext(DbContextOptions<AccountContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public AccountContext()
+        {
+        }
+    }
+}
